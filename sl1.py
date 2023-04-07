49b5c7a3b0a09f3a7c7bc212b8f602646eab8d07
Amansingh094 <129883949+Amansingh094@users.noreply.github.com>
2023-04-07 23:08:57 +0530

    Add files via upload

--- sl1.py
+++ sl1.py
@@ -1,61 +1,76 @@
 import streamlit as st
-# Header
-st.header("Mathematics for us")
-st.subheader("Hi,this website is devloped by Aman Singh and Saurav BABA of class 12th.We have created this website to help students and teachers make learning easier.")
-st.title("Fluid Mechanics")
-# Inputs
-weight = st.number_input("Enter weight (in grams/kilograms):")
-volume = st.number_input("Enter volume (in ml/l):")
-# Options
-unit_option = st.multiselect("Select output unit:", ["SI", "CGS", "Both"], default=["SI"])
-fluid_option = st.checkbox("Use custom fluid density")
-if fluid_option:
-    fluid_density = st.number_input("Enter fluid density (in kg/m^3):")
-else:
-    fluid_density = 997 # Default water density
-# Calculation
-volume = volume/1000 # Convert ml to liters
-if "SI" in unit_option or "Both" in unit_option:
-    upthrust_si: float = (weight/1000) - (fluid_density*volume)
-    st.write(f"Upthrust (SI unit): {upthrust_si} N")
-if "CGS" in unit_option or "Both" in unit_option:
-    upthrust_cgs = upthrust_si*1000
-    st.write(f"Upthrust (CGS unit): {upthrust_cgs} dyne")
-# Buoyancy
-upthrust = fluid_density*volume*9.81 # 9.81 is the acceleration due to gravity
-st.write(f"Buoyancy: {upthrust} N")
-st.write("[know more>](http://fma.if.usp.br/~eabdalla/exacta/000m1.pdf)")
+import random
+
+
+def convert_to_kg(weight_in_grams):
+    return weight_in_grams / 1000.00
+
+
+# Set page config
+st.set_page_config(
+    page_title="Mathematics for Us",
+    page_icon="🧮",
+    layout="wide"
+)
+
+# Set page background
+page_bg = '''
+<style>
+body {
+background-image: url("https://images.unsplash.com/photo-1557881365-318479f8f707");
+background-size: cover;
+}
+</style>
+'''
+st.markdown(page_bg, unsafe_allow_html=True)
+
+# Set page heading
+st.write("""
+# Mathematics for Us
+## Weight Converter
+""")
+
+# Get user input
+num_observations = st.number_input("Enter the number of observations:", min_value=1)
+
+weights_in_grams = []
+
+for i in range(num_observations):
+    weight = st.number_input(f"Enter the weight for observation {i + 1} (in grams):", min_value=1)
+    weights_in_grams.append(weight)
+
+weights_in_kg = list(map(convert_to_kg, weights_in_grams))
+
+# Display results
+st.write("### Results")
+st.write(f"The weights you entered (in grams): {weights_in_grams}")
+st.write(f"The weights converted to kilograms: {weights_in_kg}")
+
+# Cheatsheet
 # Cheatsheet
 with st.container():
     st.write("----")
     st.header("Cheat Sheet")
-    image_col, text_col = st.columns((1,1))
+    image_col, text_col = st.columns((1, 1))
     with image_col:
-           # add image here
-     with text_col:
-        st.subheader("physics cheatsheet class 11th")
-        st.markdown("[download pdf](https://www.resonance.ac.in/sc/post/attachment/(2232)-gyan-sutra-p.pdf)")
-        st.subheader("Differentiation")
-        st.markdown("[Download PDF](https://dgtstudy.com/media/media/study_material/2020-04-06/DGT_Differentiation.pdf)")
-        st.subheader("intigration")
-        st.markdown("[download pdf](https://ncert.nic.in/ncerts/l/lemh201.pdf)")
-        st.subheader("slop of line")
-        st.markdown("[download pdf](https://www.dunkirkcsd.org/cms/lib/NY19000564/Centricity/Domain/13/slope%20of%20a%20line%20notes.pdf)")
-        st.subheader("trigonometric ratios")
-        st.markdown("[download pdf](https://tutorial.math.lamar.edu/pdf/trig_cheat_sheet.pdf)")
-        st.subheader("graph")
-        st.markdown("[download pdf](file:///C:/Users/Avinash/Downloads/functions-and-graphs.pdf)")
-        st.subheader("maxima minima value function")
-        st.markdown("[download pdf](file:///C:/Users/Avinash/Downloads/functions-and-graphs.pdf)")
-#qiez
-
-
-
-
-
-
-import streamlit as st
-import random
+        with text_col:
+            st.subheader("physics cheatsheet class 11th")
+            st.markdown("[download pdf](https://www.resonance.ac.in/sc/post/attachment/(2232)-gyan-sutra-p.pdf)")
+            st.subheader("Differentiation")
+            st.markdown(
+                "[Download PDF](https://dgtstudy.com/media/media/study_material/2020-04-06/DGT_Differentiation.pdf)")
+            st.subheader("intigration")
+            st.markdown("[download pdf](https://ncert.nic.in/ncerts/l/lemh201.pdf)")
+            st.subheader("slop of line")
+            st.markdown(
+                "[download pdf](https://www.dunkirkcsd.org/cms/lib/NY19000564/Centricity/Domain/13/slope%20of%20a"
+                "%20line%20notes.pdf)")
+            st.subheader("trigonometric ratios")
+            st.markdown("[download pdf](https://tutorial.math.lamar.edu/pdf/trig_cheat_sheet.pdf)")
+            st.subheader("graph")
+            st.markdown("[download pdf](file:///C:/Users/Avinash/Downloads/functions-and-graphs.pdf)")
+            st.subheader("maxima minima value function")
+            st.markdown("[download pdf](file:///C:/Users/Avinash/Downloads/functions-and-graphs.pdf)")
 
 # Define a dictionary of quiz questions with their corresponding answers
 quiz_questions = {
@@ -63,6 +78,7 @@ quiz_questions = {
     "What is the highest mountain in the world?": ["Mount Everest", "K2", "Makalu", "Cho Oyu"],
     "What is the largest animal in the world?": ["Blue whale", "Elephant", "Giraffe", "Hippopotamus"]
 }
+
 
 # Define a function to display the quiz
 def quiz():
@@ -74,15 +90,15 @@ def quiz():
         random.shuffle(options)
         # Display the answer options as radio buttons
         selected_option = st.radio("", options, key=question)
-        # Check if an option has been selected
+
         if selected_option:
-            # Check if the selected answer is correct
+
             if selected_option == options[0]:
                 st.write("Correct!")
             else:
                 st.write("Sorry, that's incorrect.")
 
-# Define a function to create a new quiz question
+
 def create_question():
     # Get the question and answer options from the user
     question = st.text_input("Enter the quiz question:")
@@ -91,7 +107,7 @@ def create_question():
     quiz_questions[question] = options
     st.write("Question added!")
 
-# Define the main function to run the app
+
 def main():
     # Create a sidebar menu with options to take the quiz or create a new quiz question
     menu = st.sidebar.selectbox("Menu", ["Take the quiz", "Create a new quiz question"])
@@ -101,15 +117,6 @@ def main():
     elif menu == "Create a new quiz question":
         create_question()
 
-# Run the main function
+
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
